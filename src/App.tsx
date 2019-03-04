23cc5f61541c86e178b901500ae59eb745714326
Raphael Breuer <rbreuer@aeiginsurance.com>
2019-03-04 09:02:57 -0500

    new-without-error

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import './App.css';
 import { setAddressFields } from './helpers/autoFill';
+import axios from 'axios';
 import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 
 class App extends React.Component<any, any> {
@@ -14,7 +15,10 @@ class App extends React.Component<any, any> {
         zip: '',
         county: ''
       },
-      addressSearch: ''
+      addressSearch: '',
+      propertyResults: {
+        ppc: '',
+      },
     }
   }
 
@@ -30,24 +34,35 @@ class App extends React.Component<any, any> {
     if (results[0].address_components) {
       await setAddressFields(results[0].address_components, this.state.form)
       this.setState(this.state.form);
-      console.log(this.state.form);
     }
+    const result = await axios.get('https://cw3a8g5er2.execute-api.us-east-1.amazonaws.com/dev/getppccode/?city=' + this.state.form.city);
+    console.log(result.data);
   }
 
   render() {
-    const inputProps = { value: this.state.addressSearch, onChange: this.autoCompleteLocation, placeholder: 'Start Typing Address Here...' }
+    const inputProps={value: this.state.addressSearch, onChange: this.autoCompleteLocation, placeholder:'Start Typing Address Here...'}
     return (
       <div className="container text-center">
 
         <div className="card">
-        <PlacesAutocomplete inputProps={inputProps} />
+          <PlacesAutocomplete inputProps={inputProps}/>
           <div className="card-body form-group">
+            <label>Street Address</label>
             <input className="form-control" value={this.state.form.physical_address_1} name="physical_address_1" type="text" onChange={this.changeHandler} placeholder="Physical Address 1" required />
+            <label>City</label>
             <input className="form-control" value={this.state.form.city} name="city" placeholder="City" type="text" onChange={this.changeHandler} />
+            <label>State</label>
             <input className="form-control" value={this.state.form.state} name="state" type="text" placeholder="state" onChange={this.changeHandler} required />
+            <label>Zip</label>
             <input className="form-control" value={this.state.form.zip} name="zip" type="text" placeholder="zip" onChange={this.changeHandler} required />
+            <label>County</label>
             <input className="form-control" value={this.state.form.county} name="county" type="text" placeholder="county" onChange={this.changeHandler} required />
           </div>
+
+          <div className="card-footer">
+
+          </div>
+
         </div>
       </div>
 
